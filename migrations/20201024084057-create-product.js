13f18a77948adb48b3e8b06bd49f3fa58f7e8ada
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-10-24 22:45:49 +0700

    update some code

---
+++ migrations/20201024084057-create-product.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      prod_name: {
+        type: Sequelize.STRING
+      },
+      prod_desc: {
+        type: Sequelize.STRING
+      },
+      prod_price: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+var bcrypt = require('bcrypt-nodejs');
+
+module.exports = (sequelize, DataTypes) => {
+  // class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // static associate(models) {
+      // define association here
+  //   }
+  // };
+  // User.init({
+  //   username: DataTypes.STRING,
+  //   password: DataTypes.STRING
+  // }, {
+  //   sequelize,
+  //   modelName: 'User',
+  // });
+
+  const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  User.beforeSave((user, options) => {
+    if (user.changed('password')) {
+      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    }
+  });
+  User.prototype.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+  };
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+
+  return User;
+};
