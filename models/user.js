849a4b8319017c3c0183fcd8f4803f2b1b470404
sanengineer <48153104+sanengineer@users.noreply.github.com>
2020-10-30 11:20:00 +0700

    check git

--- models/user.js
+++ models/user.js
@@ -1,18 +1,16 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-var bcrypt = require('bcrypt-nodejs');
+"use strict";
+const { Model } = require("sequelize");
+var bcrypt = require("bcrypt-nodejs");
 
 module.exports = (sequelize, DataTypes) => {
   // class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    // static associate(models) {
-      // define association here
+  /**
+   * Helper method for defining associations.
+   * This method is not a part of Sequelize lifecycle.
+   * The `models/index` file will call this method automatically.
+   */
+  // static associate(models) {
+  // define association here
   //   }
   // };
   // User.init({
@@ -23,24 +21,32 @@ module.exports = (sequelize, DataTypes) => {
   //   modelName: 'User',
   // });
 
-  const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+  const User = sequelize.define(
+    "User",
+    {
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {}
+  );
   User.beforeSave((user, options) => {
-    if (user.changed('password')) {
-      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    if (user.changed("password")) {
+      user.password = bcrypt.hashSync(
+        user.password,
+        bcrypt.genSaltSync(10),
+        null
+      );
     }
   });
   User.prototype.comparePassword = function (passw, cb) {
     bcrypt.compare(passw, this.password, function (err, isMatch) {
-        if (err) {
-            return cb(err);
-        }
-        cb(null, isMatch);
+      if (err) {
+        return cb(err);
+      }
+      cb(null, isMatch);
     });
   };
-  User.associate = function(models) {
+  User.associate = function (models) {
     // associations can be defined here
   };
 
